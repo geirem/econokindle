4f8de901c7a0e37ef844d444344956e5771e8fd3
Geir Emblemsvåg <o9c@storebrand.no>
2019-08-10 11:40:08 +0200

    Refactoring parser for simplification (buggy img tags now).  And added dropcaps.

--- src/main/python/lib/Parser.py
+++ src/main/python/lib/Parser.py
@@ -6,48 +6,48 @@ class Parser:
     def __init__(self, script: dict):
         for item in [0, 1, 2, 3, 4, 5]:
             if 'canonical' in script[item]['response']:
-                self._script = script[item]['response']['canonical']
+                self.__script = script[item]['response']['canonical']
                 break
-        self._images = []
+        self.__images = []
+        self.__parsed_elements = []
 
     def __extract_main_image(self) -> Optional[str]:
-        if 'image' in self._script:
-            if 'main' in self._script['image']:
-                if 'url' in self._script['image']['main']:
-                    if 'canonical' in self._script['image']['main']['url']:
-                        return self._script['image']['main']['url']['canonical']
+        if 'image' in self.__script:
+            if 'main' in self.__script['image']:
+                if 'url' in self.__script['image']['main']:
+                    if 'canonical' in self.__script['image']['main']['url']:
+                        return self.__script['image']['main']['url']['canonical']
 
     def __find_text_data(self) -> Optional[str]:
-        for item in self._script:
+        for item in self.__script:
             if item.startswith('_text'):
                 return item
 
     def parse(self) -> dict:
         image = self.__extract_main_image()
         text_field = self.__find_text_data()
-        section = self._script['print']['section']['headline']
-        headline = self._script['headline']
-        subheadline = self._script['subheadline']
-        description = self._script['description']
-        dateline = self._script['dateline']
+        section = self.__script['print']['section']['headline']
+        headline = self.__script['headline']
+        subheadline = self.__script['subheadline']
+        description = self.__script['description']
+        dateline = self.__script['dateline']
         if dateline is None:
             dateline = ''
-        text = self._parse_html(self._script[text_field], self._images)
+        self._parse_html(self.__script[text_field])
         if image:
-            self._images.append(image)
+            self.__images.append(image)
             image = image.split('/').pop()
         result = {
             'title': self._apply_html_entities(headline),
-            'text': self._apply_html_entities(text),
+            'text': self._apply_html_entities(''.join(self.__parsed_elements)),
             'section': self._apply_html_entities(section),
             'subheadline': self._apply_html_entities(subheadline),
             'description': self._apply_html_entities(description),
             'dateline': self._apply_html_entities(dateline),
             'image': image,
-            'images': self._images,
+            'images': self.__images,
         }
         return result
-
 
     @staticmethod
     def _get_response(script: dict, index: int, field: str) -> Optional[dict]:
@@ -59,29 +59,40 @@ class Parser:
             return None
         return script[index]['response'][field]
 
-    def _parse_html(self, article_string: dict, images: list) -> str:
-        result = ''
-        for item in article_string:
+    def _parse_html(self, element: dict) -> None:
+        for item in element:
             try:
-                result += item['data']
+                self.__parsed_elements.append(item['data'])
             except:
                 pass
-            if item['type'] not in ['tag', 'html-tag']:
+            if item['type'] != 'tag':
                 continue
-            tag = item['name']
-            if tag == 'img':
-                attributes = item['attribs']
-                src = attributes['src']
-                name = src.split('/').pop()
-                images.append(src)
-                result += '<img alt="" src="' + name + '" height="' + attributes['height'] + '" width="' + attributes['width'] + '"/>'
-                continue
+            tag = self.__parse_tag_type(item)
             children = item['children']
-            if tag == 'span' or tag == 'br':
-                result += self._parse_html(children, images)
-                continue
-            result += '<' + tag + '>' + self._parse_html(children, images) + '</' + tag + '>'
-        return result
+            self.__parsed_elements.append(tag['open'])
+            self._parse_html(children)
+            self.__parsed_elements.append(tag['close'])
+
+    def __parse_tag_type(self, item: dict) -> dict:
+        tag_name = item['name']
+        tag = {
+            'name': tag_name,
+            'open': '<' + tag_name + '>',
+            'close': '</' + tag_name + '>',
+        }
+        attributes = item['attribs']
+        if tag_name == 'span':
+            if 'data-caps' in attributes and attributes['data-caps'] == 'initial':
+                tag['open'] = '<span class="dropcaps">'
+        if tag_name == 'br':
+            tag['open'] = ''
+            tag['close'] = ''
+        if tag_name == 'img':
+            src = attributes['src']
+            name = src.split('/').pop()
+            self.__images.append(src)
+            tag['open'] = '<img alt="" src="' + name + '"/>'
+        return tag
 
     @staticmethod
     def _apply_html_entities(processed: str) -> str:
