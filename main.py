c362245bc3b206494fcec21506923c7223e63b2c
Geir Emblemsvåg <geiremb@gmail.com>
2019-09-15 18:43:02 +0200

    Clean work dir between runs.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import argparse
 import datetime
 import os
 import re
+import shutil
 import sqlite3
 import subprocess
 from shutil import copyfile
@@ -120,8 +121,8 @@ def render_template(template: str, file: str, issue: dict) -> None:
 
 
 def configure_dependencies() -> list:
-    if not os.path.exists(WORK):
-        os.makedirs(WORK)
+    shutil.rmtree(WORK)
+    os.makedirs(WORK)
     args = parse_args()
     pool_manager = urllib3.PoolManager(cert_reqs='CERT_REQUIRED', ca_certs=certifi.where())
     if args.cache_key:
