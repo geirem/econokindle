1b7e1423139ddc5c302750a6d0f1b6a6876caff1
Geir Emblemsvåg <O9C@storebrand.com>
2019-10-24 22:01:53 +0200

    Drop caps are not supported on kindlegen.

--- econokindle/parsers/SpanParser.py
+++ econokindle/parsers/SpanParser.py
@@ -4,10 +4,4 @@ from econokindle.TagParser import TagParser
 class SpanParser(TagParser):
 
     def parse(self, tag: dict) -> dict:
-        attributes = tag['attribs']
-        if 'data-caps' in attributes and attributes['data-caps'] == 'initial':
-            return {
-                'open': '<span class="dropcaps">',
-                'close': '</span>',
-            }
         return super().parse(tag)
