b857383cbc03e578c717cc8cf968edb091ecf1e9
Geir Emblemsvag <geiremb@gmail.com>
2020-02-25 17:43:51 +0100

    stub test.

--- test.py
+++ test.py
@@ -1,11 +1,9 @@
 import unittest
 
 from tests.FetcherTest import FetcherTest
-from tests.KeyCreatorTest import KeyCreatorTest
 
 
 def assemble_suite():
-    # suite = unittest.load_tests(unittest.TestLoader(), unittest.TestSuite(), '.*Test.py')
     suite = unittest.TestSuite()
     suite.addTest(FetcherTest('test_that_we_skip_download_on_cache_hit'))
     return suite
@@ -13,6 +11,4 @@ def assemble_suite():
 
 if __name__ == '__main__':
     unittest.main()
-    # runner = unittest.TextTestRunner()
-    # runner.run(suite())
 
