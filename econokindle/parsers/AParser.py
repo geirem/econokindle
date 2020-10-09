6ba785b8de2e207efe08f68b1bb53ee42f2513a7
Geir Emblemsvag <geiremb@gmail.com>
2020-10-09 07:19:52 +0200

    Updated ATag parser to support empty or mailto refs.

--- econokindle/parsers/AParser.py
+++ econokindle/parsers/AParser.py
@@ -5,20 +5,24 @@ class AParser(TagParser):
 
     def parse(self, item: dict) -> dict:
         attributes = item['attribs']
+        if 'href' not in attributes:
+            return super().parse(item)
         href = self._key_creator.key(attributes['href'])
         # Hack to support references to articles in other issues.
         tag = { 'close': '</a>'}
         if href in self._valid_references:
             tag['open'] = f'<a href="#{href}">'
+            return tag
+        href = attributes['href']
+        if href.startswith('mailto:'):
+            return {'open': href, 'close': ''}
+        next_child = self._peek_at_first_child(item)
+        if 'data' not in next_child:
+            tag['open'] = '<a>'
+            return tag
+        if next_child['data'] == 'article':
+            tag['open'] = f'<a href="{href}">online '
         else:
-            href = attributes['href']
-            next_child = self._peek_at_first_child(item)
-            if 'data' not in next_child:
-                tag['open'] = '<a>'
-                return tag
-            if next_child['data'] == 'article':
-                tag['open'] = f'<a href="{href}">online '
-            else:
-                tag['open'] = f'<a href="{href}">'
-                tag['close'] = ' (online)</a>'
+            tag['open'] = f'<a href="{href}">'
+            tag['close'] = ' (online)</a>'
         return tag
